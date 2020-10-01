6e4a37cbe9022ce538ee83f65fe41bd84ddf9602
Andre Ferrer <andrerferrer@hotmail.com>
2020-10-01 10:18:26 -0300

    Finish demo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_commit :async_update # create ou update
+  after_create :send_confirmation_email # create
+
+  private 
+
+  def send_confirmation_email
+    # mandar um email de confirmação para o usuário
+  end
+
+  def async_update
+    # Síncrono
+    # UpdateUserJob.perform_now(self)
+    
+    # Assíncrono (background job)
+    UpdateUserJob.perform_later(self)
+  end
 end
